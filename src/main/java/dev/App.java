cef54da789178fa9d334e41c52713d1ecf392cca
Helvin <hjwc86@gmail.com>
2020-06-29 17:50:11 +0200

    TP3 part 1

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,14 +1,17 @@
 package dev;
+
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-	 public static void main(String[] args) {
-		    String asciiArt = FigletFont.convertOneLine("hello");
-		    System.out.println(asciiArt);
-		  }
+public class App {
+	public static void main(String[] args) {
+
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
+		System.out.println(asciiArt);
+	}
 }
