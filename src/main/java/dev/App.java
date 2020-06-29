dbdae01e617d15000d5b5290e9562156b0e9364b
Helvin <hjwc86@gmail.com>
2020-06-29 16:19:00 +0200

    V

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,4 +1,5 @@
 package dev;
+import com.github.lalyos.jfiglet.FigletFont;
 
 /**
  * Hello world!
@@ -6,8 +7,8 @@ package dev;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	 public static void main(String[] args) {
+		    String asciiArt = FigletFont.convertOneLine("hello");
+		    System.out.println(asciiArt);
+		  }
 }
